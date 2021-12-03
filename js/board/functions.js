b3eed3ff1f66018daa73c0b329b2366e32e4e8eb
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-12-03 15:30:14 +0200

    change board class code to small functions

--- js/board/functions.js
+++ js/board/functions.js
@@ -3,5 +3,5 @@ import Board from '../modules/board.js';
 export const startGame = (n) => {
     let board = new Board(n);
     board.fillBoardRandomly();
-    console.log(board.toGrid());
+    document.querySelector('.container').appendChild(board.toGrid());
 }
--- js/modules/board.js
+++ js/modules/board.js
@@ -2,39 +2,65 @@ import MineralsList from './MineralsList.js';
 
 export default class Board {
 
-    constructor(n) {
-        this.board = new Array(n);
-        for (let i = 0; i < n; i++) {
-            this.board[i] = new Array(n);
+    constructor(squaresInLine) {
+        this.squaresInLine = squaresInLine;
+        this.board = new Array(this.squaresInLine);
+        for (let i = 0; i < this.squaresInLine; i++) {
+            this.board[i] = new Array(this.squaresInLine).fill(0);
         }
     }
 
     fillBoardRandomly = () => {
-        const n = this.board.length;
-        const mineralsList = new MineralsList(n);
-        for (let i = 0; i < n; i ++) {
-            for (let j = 0; j < n; j++) {
+        const mineralsList = new MineralsList(this.squaresInLine);
+        for (let i = 0; i < this.squaresInLine; i ++) {
+            for (let j = 0; j < this.squaresInLine; j++) {
                 let mineral = mineralsList.takeRandomMineral();                    // MineralsList is empty
                 this.board[i][j] = mineral;
             }
         }
     }
 
-    
-
     toGrid = () => {
-        const grid = document.createElement('div');
-
+        const grid = this.configGrid();
+        
         for (let i = 0; i < this.board[0].length; i++) {
-            for (let j = 0; j < this.board.length; j++) {
+            for (let j = 0; j < this.squaresInLine; j++) {
                 let mineral = this.board[i][j];
-                let figure = document.createElement('figure');
-                figure.className = mineral.mineralName;
-                grid.appendChild(figure);
+                const square = this.configSquare(mineral);
+                
+                grid.appendChild(square);
             }
         }
 
         return grid;
+    }
+
+    configGrid = () => {
+        const grid = document.createElement('div');
+
+        grid.className = 'board';
+        Object.assign(grid.style, {
+            display: 'grid',
+            gridTemplateRows: `repeat(${this.squaresInLine}, auto`,
+            gridTemplateColumns: `repeat(${this.squaresInLine}, auto`,
+            width: 'fit-content',
+            height: 'fit-content'
+        });
+
+        return grid;
+    }
+
+    configSquare = (mineral) => {
+        let square = document.createElement('figure');
+        let squareSize = `${80 / this.squaresInLine}vmin`;
+
+        square.className = mineral.mineralName;
+        Object.assign(square.style, {
+            width: squareSize,
+            height: squareSize,
+        });
+
+        return square;
     }
 
     getBoard = () => {
