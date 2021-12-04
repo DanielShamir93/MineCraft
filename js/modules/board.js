19e87f1938a8491ae7e4200220f6f0d64d359956
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-12-04 13:53:30 +0200

    fixed buggs in isBoardArranged function in class Board

--- js/modules/board.js
+++ js/modules/board.js
@@ -120,53 +120,74 @@ export default class Board {
     }
 
     isBoardArranged = () => {
+
+        // inventory is not empty
+        if (this.inventoryStack.length > 0) {
+            return false;
+        }
+
         for (let i = 0; i < this.squaresInLine; i++) {
             for (let j = 0; j < this.squaresInLine; j++) {
                 let mineral = this.board[i][j];
-                
+
                 if (mineral.name === 'dirt') {
-                    if (this.downSquare() &&
-                        this.downSquare().name !== 'dirt') {
+                    if (this.downSquare(i, j) &&
+                        this.downSquare(i, j).name !== 'dirt') {
+                        console.log('from dirt')
                         // From under: There is a square but its not dirt
                         return false;
                     }
                 } else if (mineral.name === 'grass') {
-                    if (!this.downSquare() ||
-                        this.downSquare().name !== 'dirt') {
+                    if (!this.downSquare(i, j) ||
+                        this.downSquare(i, j).name !== 'dirt') {
                         // From under: There isn't a square or there is a square but its not dirt
+                        console.log('from grass')
                         return false;
                     }
                 } else if (mineral.name === 'leaves') {
-                    if ((!this.downSquare() || (this.downSquare().name ==! 'leaves' || this.downSquare().name ==! 'oak')) ||
-                        (this.upSquare() && (this.upSquare().name ==! 'leaves' || this.upSquare().name ==! 'empty'))) {
+                    if ((!this.downSquare(i, j) || (this.downSquare(i, j).name !== 'leaves' || this.downSquare(i, j).name !== 'oak')) ||
+                        (this.upSquare(i, j) && (this.upSquare(i, j).name !== 'leaves' || this.upSquare(i, j).name !== 'empty'))) {
                         // From under: There isn't a square or its not leaves or an oak ||
                         // From above: There is a square but its not leaves or empty square ||                        
+                        console.log('from leaves')
                         return false;
                     }
                 } else if (mineral.name === 'oak') {
-                    if ((!this.downSquare() || (this.downSquare().name ==! 'dirt' || this.downSquare().name ==! 'oak')) ||
-                        (this.upSquare() && (this.upSquare().name ==! 'leaves' || this.upSquare().name ==! 'oak' || this.upSquare().name ==! 'empty'))) {
-                        // From under: There isn't a square or its not a dirt or an oak
+                    if ((!this.downSquare(i, j) || (this.downSquare(i, j).name !== 'grass' || this.downSquare(i, j).name !== 'oak')) ||
+                        (this.upSquare(i, j) && (this.upSquare(i, j).name !== 'leaves' || this.upSquare(i, j).name !== 'oak' || this.upSquare(i, j).name !== 'empty'))) {
+                        // From under: There isn't a square or its not a grass or an oak
                         // From above: There is a square but its not leaves, oak or empty
+                        console.log('from oak')
                         return false;
                     }
                 } else if (mineral.name === 'stone') {
-                    if ((!this.downSquare() || (this.downSquare().name ==! 'dirt' || this.downSquare().name ==! 'stone')) ||
-                        (this.upSquare() && (this.upSquare().name ==! 'stone' || this.upSquare().name ==! 'empty'))) {
-                        // From under: There isn't a square or its not a dirt or stone
-                        // From above: There is a square but its not stone or empty
+                    if ((!this.downSquare(i, j) || (this.downSquare(i, j).name !== 'grass' || this.downSquare(i, j).name !== 'stone')) ||
+                        (this.upSquare(i, j) && (this.upSquare(i, j).name !== 'stone' || this.upSquare(i, j).name !== 'leaves' || this.upSquare(i, j).name !== 'empty'))) {
+                        // From under: There isn't a square or its not a grass or stone
+                        // From above: There is a square but its not stone or leaves or empty
+                        console.log('from stone')
                         return false;
                     }
                 }
             }
         }
+        // The board arranged properly
+        return true;
     }
 
     downSquare = (i, j) => {
+        if (i === this.squaresInLine - 1) {
+            // Exceeded the board bottom border
+            return false;
+        }
         return this.board[i+1][j];
     }
 
     upSquare = (i, j) => {
+        if (i === 0) {
+            // Exceeded the board top border
+            return false;
+        }
         return this.board[i-1][j];
     }
 }
