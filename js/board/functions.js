76c3574c3c3d797fd649e85e1460a09481774202
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-12-04 18:34:22 +0200

    adding timer, score and comment area

--- js/board/functions.js
+++ js/board/functions.js
@@ -5,9 +5,9 @@ let board = null;
 const setGame = (n) => {
     if (n > 4) {
         board = new Board(n);
-        let container = document.querySelector('.container');
+        const container = document.querySelector('.container');
         board.fillBoardRandomly();
-        container.insertBefore(board.toGrid(), container.firstChild);
+        container.insertBefore(board.toGrid(), container.children[1]);
     } else {
         alert('Board must have at least 5 rows and columns');
     }
