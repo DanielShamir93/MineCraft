bab02a58a5526e595157d284e338279e2065e0f8
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-12-05 09:37:46 +0200

    fix next level bug

--- js/opening/listeners.js
+++ js/opening/listeners.js
@@ -3,7 +3,7 @@ import { setGame } from '../board/functions.js';
 
 const playListener = document.querySelector('.play').addEventListener('click', (e) => {
     document.querySelector('.opening').style.display = 'none';
-    const lineSquareNumber = document.querySelector('#line-square-number').value;
+    const lineSquareNumber = parseInt(document.querySelector('#line-square-number').value);
     setGame(lineSquareNumber);
 });
 
